ece7ed825fdb1219bd07c98c431484ab17ce37dd
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-05-27 11:14:02 -0700

    update

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const fs = require('fs')
 const path = require("path")
 const Files = require("../models/files.model");
+const { type } = require("express/lib/response");
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -20,32 +21,41 @@ const fileUpload = multer({
 })
 
 router.post('/upload', fileUpload.single('FILE_UPLOAD'), (req, res)=> {
+
     const numbGen = Math.floor(Math.random() * 99999999) + 10000000
+    const mimetype = req.file.mimetype.split("/")
     if(req.fileValidationError) {
         res.redirect('/')
     }
 
-    const file = new Files({
-        file: {
-            data: fs.readFileSync(path.join('./uploads/' + req.file.filename)),
-            contentType: req.file.mimetype
-        },
-        file_id: numbGen,
-        uploader_ip: req.ip
-    })
+    if(mimetype[0] !== "image" || mimetype[0] !== "video") {
+        res.redirect("/")
+        console.log(`> cannot upload ${mimetype[0]}`)
+    } else {
 
-    file.save((err)=> {
-        if(err) throw err;
-        console.log("> " + req.ip + " has uploaded: " + req.file.originalname)
-        Files.findOne({"file_id": numbGen}, (err, data) => {
-            if(err) throw err
-            if(!data) {
-                res.redirect('/')
-            } else {
-                res.redirect('/file/' + data.file_id)
-            }
+        const file = new Files({
+            file: {
+                data: fs.readFileSync(path.join('./uploads/' + req.file.filename)),
+                contentType: req.file.mimetype
+            },
+            file_id: numbGen,
+            uploader_ip: req.ip
         })
-    })
+    
+        file.save((err)=> {
+            if(err) throw err;
+            console.log("> " + req.ip + " has uploaded: " + req.file.originalname)
+            Files.findOne({"file_id": numbGen}, (err, data) => {
+                if(err) throw err
+                if(!data) {
+                    res.redirect('/')
+                } else {
+                    res.redirect('/file/' + data.file_id)
+                }
+            })
+        })
+
+    }
 
 })
 
